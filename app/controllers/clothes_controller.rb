bb679ab0e4f6fdefd7484a77c38554c8879e2814
theo denis <theo.laos2@gmail.com>
2022-01-27 19:52:46 +0100

    controller wéwéwé

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -1,2 +1,45 @@
 class ClothesController < ApplicationController
+  before_action :find_clothe, only: [ :show, :destroy ]
+  before_action :new_clothe, only: [ :new, :create ]
+
+  def new
+    # @clothe = Clothe.new(review_params)
+  end
+
+  def destroy
+    @clothe.destroy
+    # redirect_to clothes_path
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @clothe.user = @user
+    if @clothe.save
+      # redirect_to clothe_path(@clothe)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def index
+    Clothe.all
+  end
+
+  private
+
+  def find_clothe
+    @clothe = Clothe.find(params[:id])
+  end
+
+  def review_params
+    params.require(:user).permit(:height :body_shape :skin_tone :waist_circ :chest_circ :hip_circ)
+  end
+
+  def new_clothe
+    @review = Review.new(review_params)
+  end
+
 end
