8bae839e6d9257e719983bb6e7c65af5a88f9b38
Christelle Lecamp <christelle.lecamp@gmail.com>
2022-01-29 12:08:47 +0100

    page avatar ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'avatar', to: 'pages#avatar'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:show]
+
 
 
   resources :clothes, only: [:show, :index, :new]
