c154809a877c46a8c1f6cfe82f0d7b48b64dde29
leo kassabi <leokassabi@gmail.com>
2022-02-15 19:22:19 +0100

    routes index show et new

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -8,4 +8,13 @@ class ShopsController < ApplicationController
     @shop = Shop.find(param[:id])
   end
 
+  def new
+    @shop = Shop.new(shop_params)
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:brand_name, :description)
+  end
 end
