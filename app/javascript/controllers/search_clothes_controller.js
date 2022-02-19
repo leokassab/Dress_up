f2863c3d0b1ee571a7e93da6f8853dfae26a74bb
theo denis <theo.laos2@gmail.com>
2022-02-19 12:26:10 +0100

    stimulus posée sur index et avatar

--- app/javascript/controllers/search_clothes_controller.js
+++ app/javascript/controllers/search_clothes_controller.js
@@ -1,13 +1,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['form', 'input', 'list'];
-
-  connect() {
-    console.log(this.formTarget);
-    console.log(this.inputTarget);
-    console.log(this.listTarget);
-  }
+  static targets = ['form', 'input', 'list', 'filters', 'searchbar', 'cross', 'avatar'];
 
   update() {
     const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
@@ -16,5 +10,20 @@ export default class extends Controller {
       .then((data) => {
         this.listTarget.outerHTML = data;
       })
+  }
+
+  displayFilters() {
+    console.log("coucou")
+    this.filtersTarget.classList.toggle('d-none');
+    this.crossTarget.classList.toggle('d-none')
+    if (this.hasSearchbarTarget) {
+      this.searchbarTarget.classList.toggle("d-none");
+    }
+    if (this.hasListTarget) {
+      this.listTarget.classList.toggle('d-none');
+    }
+    if (this.hasAvatarTarget) {
+      this.avatarTargets.forEach(avatar => avatar.classList.toggle('d-none') );
+    }
   }
 }
